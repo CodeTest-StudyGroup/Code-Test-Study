18bad908761f0be2a08dc217d39a438659a6110c
Hyunwoo Kim <37402136+qrlagusdn@users.noreply.github.com>
2020-04-07 10:55:39 +0900

    Update 1949.py

--- qrlagusdn/1949.py
+++ qrlagusdn/1949.py
@@ -1,46 +1,55 @@
 #1949 "등산로 조성"
 #문자열에서는 find 되는데 정수 리스트에서는 find 안됨...그래서 인덱스 찾으려면 index를 써야한다.
-
-def Insafe(old_y,old_x,y,x):
-    global flag
-    if 0<=y<N and 0<=x<N:#먼저 좌표안에 있어야한다.
-        if MAP[old_x][old_y] > MAP[x][y]: #이전의 봉우리보다 낮아야한다.
-            return 1 #y,x 로 움직일 수 있다.
-        else:#만약 낮지않다면
-            #MAP[x][y]를 MAP[old_x][old_y]보다 딱 한 칸 낮게 만들면 된다.
-            
-            depth = MAP[x][y] - MAP[old_x][old_y] + 1
-            if MAP[x][y] == 0: #더 이상 못깎을 때
-                return 0
-            if flag == 1 and K >= depth:
-                #봉우리를 낮출 수 있다면
-                #낮출 수도 있고 k의 값이 충분하다.
-                flag == 1 
-                MAP[x][y] -=depth
-                return 1
-
-            else:#k의 값이 충분하지 못하거나 이미 깎았다면.
-                return 0 #못간다.
-
-
-def dfs(y,x):
-    global Distance
+ 
+ 
+def dfs(Distance, y,x,visited):
     global MaxDistance
-    visited.append((y,x))
+    global K
+    global flag
+     
     for dir in range(0,4):
-        new_y = y + dx[dir]
-        new_x = x + dy[dir]
-
-        if new_y  == start_y and new_x == start_x:#원점으로 돌아오면 distance 길이 비교해서 초기화
-            if Distance > MaxDistance:
-                MaxDistance = Distance
-            Distance = 0
-
-        if Insafe(y,x,new_y, new_x) and (new_y, new_x) not in visited:
-            Distance += 1 #y,x로 가면 distance 가 늘어난다.
-            dfs(new_y,new_x)
-
-    
+        new_y = y + dy[dir]
+        new_x = x + dx[dir]
+            #Insafe(y,x,, new_x) 
+        if 0<=new_y MAP[new_y][new_x]: #이전의 봉우리보다 낮아야한다.
+            #y,x 로 움직일 수 있다.
+                visited.append((new_y,new_x))
+                Distance += 1
+                dfs(Distance, new_y,new_x,visited)
+ 
+                if Distance >= MaxDistance:
+                    MaxDistance = Distance
+ 
+                ############되돌리는 부분
+                Distance -= 1
+                visited.pop()
+         
+            else :#만약 낮지않다면
+                #MAP[x][y]를 MAP[old_x][old_y]보다 딱 한 칸 낮게 만들면 된다.
+                 
+                depth = MAP[new_y][new_x] - MAP[y][x] + 1
+ 
+                if flag == 1 and K >= depth and MAP[new_y][new_x] != 0:
+                    #봉우리를 낮출 수 있다면
+                    #낮출 수도 있고 k의 값이 충분하다.
+                    flag = 0
+                    MAP[new_y][new_x] -= depth
+ 
+                    visited.append((new_y,new_x))
+ 
+                    Distance += 1
+                    dfs(Distance, new_y,new_x,visited)
+ 
+                    if Distance >= MaxDistance:
+                        MaxDistance = Distance
+ 
+                    ############되돌리는 부분
+                    visited.pop()
+                    Distance -=1
+                    MAP[new_y][new_x] += depth #map 부분 다시 되돌리기
+                    flag = 1
+ 
+     
 T = int(input())
 for TC in range(1, T + 1):
     flag = 1 #지형 깎는 공사 진행했는지 안했는지
@@ -53,32 +62,23 @@ for TC in range(1, T + 1):
         MAP.append(arr)
         if top <= max(arr):
             top = max(arr)
-
+ 
     for y in range(0,N):
         for x in range(0,N):
             if MAP[y][x] == top:
-                topList.append((x,y))
-
+                topList.append((y,x))
+ 
     dy = [-1,1,-0,0]
     dx = [0,0,-1,1]
     MaxDistance = -999
-    
-    if topList: #봉우리 개수만큼 출발점
+     
+    j = len(topList)
+    for i in range(j): #봉우리 개수만큼 출발점
         visited = list()
-        Distance = 0
+        Distance = 1
         start = topList.pop()
         start_y, start_x = start[0],start[1]
-        dfs(start_y,start_x)
-
-    print(MaxDistance)
-
-    '''
-10
-5 1
-9 3 2 3 2
-6 3 1 7 5
-3 4 8 9 9
-2 3 7 7 7
-7 6 5 5 8
-    
-    '''
+        visited.append((start_y, start_x))#시작점 추가
+ 
+        dfs(Distance, start_y,start_x,visited)  
+    print("#%d %d" %(TC,MaxDistance))

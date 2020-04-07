dc90f5217d3802ef62f254c999fe5d5fcce36a90
Hyunwoo Kim <37402136+qrlagusdn@users.noreply.github.com>
2020-04-07 10:54:35 +0900

    Update 15997.py

--- qrlagusdn/15997.py
+++ qrlagusdn/15997.py
@@ -1,51 +1,87 @@
 #카카오 코드페스티벌 승부 예측 
 #https://www.acmicpc.net/problem/15997
+#브루트포스 알고리즘 공부하기
+#DFS 다시 공부하기
+#소팅하는 부분 다시 공부
+#->
+
 import sys
 
-name1, name2, name3, name4 = map(str,input().split())
-nameDic = {name1:0,name2:0,name3:0,name4:0 }
-#나라이름 입력받기
+def dfs(cnt,scoreDic, P):
+    if cnt == 6:#소팅하는부분
+        sortDic = sorted(list(scoreDic.items()), key = lambda k : k[1], reverse = True)
+        if sortDic[0][1] == sortDic[1][1] == sortDic[2][1] == sortDic[3][1]: #상위 4개가 똑같은경우 0.25, 0.25, 0.25. 0.25 4명동점
+            ansDic[sortDic[0][0]] += P*0.5
+            ansDic[sortDic[1][0]] += P*0.5
+            ansDic[sortDic[2][0]] += P*0.5
+            ansDic[sortDic[3][0]] += P*0.5
 
-for i in range(6):#승점 확률 계산
-    string = list(map(str,input().split()))
-    A = string[0]
-    B = string[1]
-    W, D, L = float(string[2]), float(string[3]), float(string[4])
+        elif sortDic[0][1] == sortDic[1][1] == sortDic[2][1] > sortDic[3][1] :#상위 3개가 똑같은 경우 0.3333,0.3333,0.3333, 0 3명동점
+            ansDic[sortDic[0][0]] += P*(2/0.3)
+            ansDic[sortDic[1][0]] += P*(2/0.3)
+            ansDic[sortDic[2][0]] += P*(2/0.3)
+            ansDic[sortDic[3][0]] += P*0.0
 
-    nameDic[A] += W*3 + D*1
-    nameDic[B] += L*3 + D*1
+        elif sortDic[0][1] > sortDic[1][1] == sortDic[2][1] == sortDic[3][1] :#2 3 4 등이 똑같을 수도 3명동점
+            ansDic[sortDic[0][0]] += P
+            ansDic[sortDic[1][0]] += P*(1/0.3)
+            ansDic[sortDic[2][0]] += P*(1/0.3)
+            ansDic[sortDic[3][0]] += P*(1/0.3)
 
-ansDic = {name1:0.0,name2:0.0,name3:0.0,name4:0.0} #본선진출 확률 대입할 리스트
-sortDic = sorted(nameDic, key = lambda k : nameDic[k], reverse = True)# 승점확률에 따라 내림차순 정렬
+        elif sortDic[0][1] > sortDic[1][1] == sortDic[2][1] > sortDic[3][1]:#2위 3위가 똑같은 경우 1, 0.5, 0.5, 0 2명동점
+            ansDic[sortDic[0][0]] += P
+            ansDic[sortDic[1][0]] += P*0.5
+            ansDic[sortDic[2][0]] += P*0.5
+            ansDic[sortDic[3][0]] += P*0.0
 
-if nameDic[sortDic[0]] == nameDic[sortDic[1]] and nameDic[sortDic[0]] == nameDic[sortDic[2]] and nameDic[sortDic[0]] == nameDic[sortDic[3]] :#상위 4개가 똑같은경우 0.25, 0.25, 0.25. 0.25 4명동점
-    ansDic[sortDic[0]] = nameDic[sortDic[0]]*0.25
-    ansDic[sortDic[1]] = nameDic[sortDic[1]]*0.25
-    ansDic[sortDic[2]] = nameDic[sortDic[2]]*0.25
-    ansDic[sortDic[3]] = nameDic[sortDic[3]]*0.25
-elif nameDic[sortDic[0]] == nameDic[sortDic[1]] and nameDic[sortDic[0]] == nameDic[sortDic[2]] :#상위 3개가 똑같은 경우 0.3333,0.3333,0.3333, 0 3명동점
-    ansDic[sortDic[0]] = nameDic[sortDic[0]]*(1/0.3)
-    ansDic[sortDic[1]] = nameDic[sortDic[1]]*(1/0.3)
-    ansDic[sortDic[2]] = nameDic[sortDic[2]]*(1/0.3)
-    ansDic[sortDic[3]] = nameDic[sortDic[3]]*0.0
-elif nameDic[sortDic[1]] == nameDic[sortDic[2]] and nameDic[sortDic[1]] == nameDic[sortDic[3]]:#2 3 4 등이 똑같을 수도 3명동점
-    ansDic[sortDic[0]] = nameDic[sortDic[0]]*1.0
-    ansDic[sortDic[1]] = nameDic[sortDic[1]]*(1/0.3)
-    ansDic[sortDic[2]] = nameDic[sortDic[2]]*(1/0.3)
-    ansDic[sortDic[3]] = nameDic[sortDic[3]]*(1/0.3)
-elif nameDic[sortDic[1]] == nameDic[sortDic[2]]:#2위 3위가 똑같은 경우 1, 0.5, 0.5, 0 2명동점
-    ansDic[sortDic[0]] = nameDic[sortDic[0]]*1.0
-    ansDic[sortDic[1]] = nameDic[sortDic[1]]*0.5
-    ansDic[sortDic[2]] = nameDic[sortDic[2]]*0.5
-    ansDic[sortDic[3]] = nameDic[sortDic[3]]*0.0
-else: #그 외는 1, 1, 0 ,0  
-    # 1=2,3,4 / 1,2,3=4/ 1,2,3,4
-    ansDic[sortDic[0]] = nameDic[sortDic[0]]*1.0
-    ansDic[sortDic[1]] = nameDic[sortDic[1]]*1.0
-    ansDic[sortDic[2]] = nameDic[sortDic[2]]*0.0
-    ansDic[sortDic[3]] = nameDic[sortDic[3]]*0.0
+        else: #그 외는 1, 1, 0 ,0  
+            # 1=2,3,4 / 1,2,3=4/ 1,2,3,4
+            ansDic[sortDic[0][0]] += P
+            ansDic[sortDic[1][0]] += P
+            ansDic[sortDic[2][0]] += P*0.0
+            ansDic[sortDic[3][0]] += P*0.0
+        
+        return
 
-print("%.10f" %(ansDic[name1]))
-print("%.10f" %(ansDic[name2]))
-print("%.10f" %(ansDic[name3]))
-print("%.10f" %(ansDic[name4]))
+    else:
+        #A가 이길 때
+        scoreDic[stringList[cnt][0]] += 3 #A    
+
+        dfs(cnt+1, scoreDic, P*float(stringList[cnt][2]))#dfs 파라미터에 scoreDic이 들어가는 이유를 제대로 알자!!
+        
+        scoreDic[stringList[cnt][0]] -= 3 #A
+
+        #비길때
+        scoreDic[stringList[cnt][0]] += 1 #A
+        scoreDic[stringList[cnt][1]] += 1 #B
+
+        dfs(cnt+1,scoreDic, P*float(stringList[cnt][3]))
+        
+        scoreDic[stringList[cnt][0]] -= 1 #A
+        scoreDic[stringList[cnt][1]] -= 1 #B
+
+        #A가 질 때
+        scoreDic[stringList[cnt][1]] += 3 #B
+
+        dfs(cnt+1,scoreDic, P*float(stringList[cnt][4]))
+        
+        scoreDic[stringList[cnt][1]] -= 3 #B
+
+
+if __name__ == "__main__":
+    name1, name2, name3, name4 = map(str,input().split())
+    #나라이름 입력받기
+    stringList = list()
+
+    for i in range(6):
+        string = list(map(str,input().split()))
+        stringList.append(string)
+
+
+    scoreDic = {name1:0,name2:0,name3:0,name4:0} 
+    ansDic = {name1:0.0,name2:0.0,name3:0.0,name4:0.0} #본선진출 확률 대입할 리스트
+
+    dfs(0, scoreDic, 1)
+
+    for key in scoreDic:
+        print("%6lf" %ansDic[key])

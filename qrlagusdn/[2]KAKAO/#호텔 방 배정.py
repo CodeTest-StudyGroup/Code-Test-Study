c8a704b1cb385a02e9afac8ab7c401b0f8e4822e
Hyunwoo Kim <37402136+qrlagusdn@users.noreply.github.com>
2020-04-23 14:08:49 +0900

    호텔 방 배정 _ 효율성 에러

---
+++ qrlagusdn/[2]KAKAO/#호텔 방 배정.py
@@ -0,0 +1,27 @@
+#호텔 방 배정
+
+'''
+비트마스크로 접근해서 답은 다 나오는데 효율성을 다 터진다.
+78점 나옴..
+
+다른 방법도 생각해봐야겟다.
+
+'''
+
+def solution(k, room_number):
+    answer = []
+    room = 0
+    for i in range(0,len(room_number)):
+        n = room_number[i]
+        j = -1
+        while 1:
+            if n+j >= k: break #배정받을 수 있는 경우만 주어진다고 했으니깐 고려안해도 될 듯
+            if room & (1<<n+j) == 0:#방이 비어있다.
+                room = room|(1<<n+j) #채워줌
+                answer.append(n+j+1)
+                break
+            j += 1
+    return answer
+
+if __name__ == "__main__":
+    print(solution(10,[1,3,4,1,3,1]))

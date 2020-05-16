57599060e552d2093fc312d156a9896a6f8e096e
Hyunwoo Kim <37402136+qrlagusdn@users.noreply.github.com>
2020-05-17 02:06:20 +0900

    Update #입국심사.py

--- qrlagusdn/[3]Programmers/#입국심사.py
+++ qrlagusdn/[3]Programmers/#입국심사.py
@@ -1,37 +1,27 @@
 #입국심사
 
+#넘어려운뎅...,?
+
 def solution(n, times):
-    answer = 0
-    sorted(times)
-    table = [[0,times[i]] for i in range(0,len(times))]
-    time = 0
-    front = 0
-    rear = len(times)-1
-    arr = [i for i in range(0,len(table))]
-    while n>0:
-        for i in range(0,len(table)):
-            if table[i][0] > 0: #안에 사람이 진행중이면
-                table[i][0] -= 1
-                if table[i][0] == 0:
-                    arr.append(i)
-        sorted(arr)
+    answer = []
+    high = max(times) * n
+    low = 0
+    answer = 99999999999999999
+    while 1:
+        if high-low==1: break
+        mid = (high+low)//2
+        tmp = 0
+        for t in times:
+            tmp += mid//t
 
-        while 1:
-            if not arr or n == 0:
-                break
-            if n == 1:
-                table = sorted(table,key=lambda k: (k[0]+k[1]))
-                time += table[0][0]+table[0][1]
-                return time
-            idx = arr.pop(0)
-            table[idx][0] = table[idx][1]
-            n -=1
-
-        table = sorted(table,key=lambda k: (k[0]+k[1]))
-        
-        time += 1
-            
-    return time
+        if tmp>=n:
+            high = mid
+            answer = min(answer,mid)
+        else:
+            low = mid
+    
+    return answer
+    
 if __name__ == "__main__":
 
-    print(solution(6,[7, 10]))
+    print(solution(6,[7, 10]	))

4c22eb9b212984e28b564b46a3b5582f283570fc
Kuyho Chung <kchung1995@gmail.com>
2021-06-29 22:00:44 +0900

    가장 큰 정사각형

---
+++ kuyhochung/1. 매주 문제풀이/41주차/1915_가장 큰 정사각형.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+#define MAX_N 1001
+
+int n, m;
+int board[MAX_N][MAX_N];
+
+
+int answer = 0;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) {
+		string temp;
+		cin >> temp;
+		for (int j = 1; j <= m; j++) {
+			board[i][j] = temp[j - 1] - '0';
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (board[i][j] == 0) continue;
+			board[i][j] = min(board[i - 1][j], min(board[i - 1][j - 1], board[i][j - 1])) + 1;
+			answer = max(answer, board[i][j]);
+		}
+	}
+
+	cout << answer * answer;
+	return 0;
+}
